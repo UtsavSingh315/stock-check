8dc9ba33c213378964b1402ce53d592ad5d87cf0
Utsav Singh <84584429+UtsavSingh315@users.noreply.github.com>
2024-10-14 18:57:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import streamlit as st
 from datetime import date
 from prophet import Prophet
 import yfinance as yf
-# from fbprophet import Prophet
-# from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 START = "2015-01-01"
@@ -33,7 +31,7 @@ data_load_state.text('Loading data... done!')
 st.subheader('Raw data')
 st.write(data.tail())
 
-# Plot raw data
+
 def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
@@ -43,7 +41,7 @@ def plot_raw_data():
 	
 plot_raw_data()
 
-# Predict forecast with Prophet.
+
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
@@ -52,14 +50,11 @@ m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
-# Show and plot forecast
+
 st.subheader('Forecast data')
 st.write(forecast.tail())
     
 st.write(f'Forecast plot for {n_years} years')
-# fig1 = plot_plotly(m, forecast)
-# st.plotly_chart(fig1)
-
 st.write("Forecast components")
 fig2 = m.plot_components(forecast)
 st.write(fig2)
